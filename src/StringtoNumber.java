0a89756f0ce34978b8f6076070fd7d48deb24dcf
Shmaila Rehman <snrehman16@gmail.com>
2019-10-14 12:49:29 +0100

    String to number

---
+++ src/StringtoNumber.java
@@ -0,0 +1,7 @@
+public class StringtoNumber {
+    public static void main(String[] args) {
+     String number = "20";
+     int result = Integer.parseInt(number);
+        System.out.println(result);
+    }
+}
